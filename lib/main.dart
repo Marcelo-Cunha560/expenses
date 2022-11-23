ebb2a5ce4e2e9b76de7e1a5cd44891f638b2630c
Marcelo-CunhaDev <matheusluc22@gmail.com>
2022-11-23 10:40:23 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:expenses/transation.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final _Transaction = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tenis de Cprrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Despesas pessoais'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: const Card(
+              color: Colors.blue,
+              child: Text('Gráfico'),
+              elevation: 5,
+            ),
+          ),
+          Column(
+              children: _Transaction.map((tr) {
+            return Card(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 15, vertical: 10),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      color: Colors.black,
+                      width: 2,
+                    )),
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      'R\$ ${tr.value.toStringAsFixed(2)}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      ),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        tr.title,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        DateFormat('d MMM y').format(tr.date),
+                        style: const TextStyle(
+                            color: Color.fromARGB(162, 79, 79, 79)),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            );
+          }).toList())
+        ],
+      ),
+    );
+  }
+}
