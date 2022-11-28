661981961e64c4bbd0b8c208f07c5ac2760cb3ac
Marcelo-CunhaDev <matheusluc22@gmail.com>
2022-11-28 14:56:18 -0400

    att

---
+++ lib/components/transition_list.dart
@@ -0,0 +1,20 @@
+// import 'package:flutter/material.dart';
+// import '..models/transaction.dart';
+
+
+// class TransationList extends StatelessWidget {
+  
+  
+//   final List<Transation> transactions;
+
+//   TransationList(this.transactions)
+
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+
+
+//     );
+//   }
+// }
